fd0b1ffaff4729c2c6b83edae42145b3dd095fce
SalsabelDalaq <95722891+SalsabelDalaq@users.noreply.github.com>
2023-12-08 20:53:17 -0600

    Add files via upload

---
+++ ShortestPath.h
@@ -0,0 +1,19 @@
+// ShortestPath.h
+#ifndef SHORTESTPATH_H
+#define SHORTESTPATH_H
+
+#include "Graph.h"
+#include <unordered_map>
+#include <queue>
+#include <string>
+
+class ShortestPath {
+private:
+    Graph graph;
+
+public:
+    ShortestPath(const Graph& g);
+    std::vector<std::string> findShortestPath(const std::string& start, const std::string& end);
+};
+
+#endif // SHORTESTPATH_H
